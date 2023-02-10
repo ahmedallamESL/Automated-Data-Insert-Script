a05ce162ca7f6d8b6c44ae85b912c2f0329244ba
anglelive23 <anglelive23@gmail.com>
2023-02-10 13:20:51 +0200

    refactoring PopulateAllLevelsWithoutLevelOne and PupulateAllLevelsIncludingLevelOne to use Switch expression instead of ugly nasted if statments

--- AutomatedDataInsert/Worker.cs
+++ AutomatedDataInsert/Worker.cs
@@ -40,13 +40,20 @@ namespace AutomatedDataInsert
                 for (int i = 2; i <= 4; i++)
                 {
                     Console.WriteLine($"{initialPressure}\t\t{Altitude}\t\t{i}");
-                    if (i == 2)
-                        Altitude += levelThreeFractionFromLevelTwo;
-                    if (i == 3)
+
+                    var altitudeCase = i switch
                     {
-                        levelFourInitialAltitudeValue += 36;
-                        Altitude = levelFourInitialAltitudeValue;
-                    }
+                        2 => Altitude += levelThreeFractionFromLevelTwo,
+                        3 => Altitude = (levelFourInitialAltitudeValue+=36),
+                        _ => Altitude += 0,
+                    };
+                    //if (i == 2)
+                    //    Altitude += levelThreeFractionFromLevelTwo;
+                    //if (i == 3)
+                    //{
+                    //    levelFourInitialAltitudeValue += 36;
+                    //    Altitude = levelFourInitialAltitudeValue;
+                    //}
                 }
                 initialPressure += 5;
                 Altitude = initialAltitude + (altitudeFraction * iterationIndex++);
@@ -71,17 +78,26 @@ namespace AutomatedDataInsert
                 for (int i = 1; i <= 4; i++)
                 {
                     Console.WriteLine($"{pressure}\t\t{altitude}\t\t{i}");
-                    if (i == 1 && pressure == 985)
-                        altitude = levelTwoInitialAltitudeValue;
-                    if (i == 1 && pressure != 985)
-                        altitude += levelTwoFractionFromLevelOne;
-                    if (i == 2)
-                        altitude += levelThreeFractionFromLevelTwo;
-                    if (i == 3)
+
+                    var altitudeCase = i switch
                     {
-                        levelFourInitialAltitudeValue += firstAltitudeFraction;
-                        altitude = levelFourInitialAltitudeValue;
-                    }
+                        1 => altitude = (pressure == 985) ? levelTwoInitialAltitudeValue : (altitude + levelTwoFractionFromLevelOne),
+                        2 => altitude += levelThreeFractionFromLevelTwo,
+                        3 => altitude = (levelFourInitialAltitudeValue += firstAltitudeFraction),
+                        _ => altitude += 0,
+                    };
+
+                    //if (i == 1 && pressure == 985)
+                    //    altitude = levelTwoInitialAltitudeValue;
+                    //if (i == 1 && pressure != 985)
+                    //    altitude += levelTwoFractionFromLevelOne;
+                    //if (i == 2)
+                    //    altitude += levelThreeFractionFromLevelTwo;
+                    //if (i == 3)
+                    //{
+                    //    levelFourInitialAltitudeValue += firstAltitudeFraction;
+                    //    altitude = levelFourInitialAltitudeValue;
+                    //}
                 }
                 altitude = (pressure == 985) ? (initialAltitude + firstAltitudeFraction) : ((initialAltitude + firstAltitudeFraction) + (altitudeFraction * iterationIndex++));
                 pressure += 5;
